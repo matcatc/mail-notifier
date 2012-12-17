484b4545f62f782385e3f3feebfa58ac0e09e2af
Matthew Todd <matcatprg@yahoo.com>
2012-12-16 17:30:00 -0800

    Added license information (GPL v. 3)

--- mail_notifier.py
+++ mail_notifier.py
@@ -4,8 +4,6 @@ Simple script which notifies user when a new mail comes in.
 
 Works by keeping track of unread mail and notifying user when it increases.
 
-TODO: license (GPL v. 3)
-
 TODO: program arguments
     config file?
 
@@ -13,6 +11,20 @@ TODO: handle import errors for notify2?
     Continue running and use another method vs quiting?
 
 TODO: logging?
+
+@license
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @author Matthew Todd
 @date Dec 16, 2012
