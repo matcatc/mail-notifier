c48eae67fc6bdbed05d369a3637242673ed59f50
Matthew Todd <matcatprg@yahoo.com>
2012-12-16 16:58:41 -0800

    Handle case where claws-mail not running

--- mail_notifier.py
+++ mail_notifier.py
@@ -4,13 +4,18 @@ Simple script which notifies user when a new mail comes in.
 
 Works by keeping track of unread mail and notifying user when it increases.
 
+TODO: license (GPL v. 3)
+
 TODO: handle case where mail client not running
     so can't get numbers from it
 
 TODO: program arguments
+    config file?
 
 TODO: handle import errors for notify2?
     Continue running and use another method vs quiting?
+
+TODO: logging?
 
 @author Matthew Todd
 @date Dec 16, 2012
@@ -27,6 +32,8 @@ import notify2
 # constants
 SLEEP_TIME = 10
 
+# msg claws-mail output when its not running. Unlikely to change, but could.
+CLAWS_MAIL_NOT_RUNNING = '0 Claws Mail not running.'
 
 
 class mail_info:
@@ -86,9 +93,13 @@ def get_number_mail():
     This function is dependent on what mail client you use. I use claws and to
     keep it simple, I just hard-coded it.
 
-    @return mail_info
+    @return mail_info if was able to get the data. None if not.
     '''
     data = subprocess.check_output(['claws-mail', '--status'], universal_newlines=True)
+
+    if data.strip() == CLAWS_MAIL_NOT_RUNNING:
+        return None
+
     data = tuple(map(int, data.split()))
     return mail_info(data[0], data[1], data[2])
 
@@ -112,17 +123,18 @@ def notify(diff_new, diff_unread):
 def main():
     notify2.init('mail_notifier')
 
-    prev_number = get_number_mail()
+    prev_number = None
 
     while True:
         curr_number = get_number_mail()
 
-        if any((curr_number > prev_number)[:2]):
-            diff_number = curr_number - prev_number
-            diff_new = max(0, diff_number.new)
-            diff_unread = max(0, diff_number.unread)
+        if not(prev_number is None or curr_number is None):
+            if any((curr_number > prev_number)[:2]):
+                diff_number = curr_number - prev_number
+                diff_new = max(0, diff_number.new)
+                diff_unread = max(0, diff_number.unread)
 
-            notify(diff_new, diff_unread)
+                notify(diff_new, diff_unread)
 
         prev_number = curr_number
         time.sleep(SLEEP_TIME)
